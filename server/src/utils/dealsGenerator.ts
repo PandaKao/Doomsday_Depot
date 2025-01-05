c65dc82c025a22b60f1216796d7fc2f8b1d757aa
Justin Kao <justinkao010@gmail.com>
2025-01-05 13:17:11 -0800

    fixed dealsGenerator

--- server/src/utils/dealsGenerator.ts
+++ server/src/utils/dealsGenerator.ts
@@ -1,20 +1,23 @@
 import cron from "node-cron";
 import { Product } from "../models/index.js";
 
-console.log("cron running");
-cron.schedule('0 0 * * *', async () => {
-    try {
-        const expiredProducts = await Product.find({ onSaleDate: { $lt: new Date() } });
-        const randomProducts = expiredProducts.sort(() => 0.5 - Math.random()).slice(0, 9);
+export const dealsUpdate = () => {
+    console.log("cron running");
 
-        const updatedProducts = await Promise.all(randomProducts.map(product =>
-            Product.findByIdAndUpdate(product._id, {
-                onSaleDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-            }, { new: true })
-        ));
+    cron.schedule('0 0 * * *', async () => {
+        try {
+            const expiredProducts = await Product.find({ onSaleDate: { $lt: new Date() } });
+            const randomProducts = expiredProducts.sort(() => 0.5 - Math.random()).slice(0, 9);
 
-        console.log("Updated products: ", updatedProducts);
-    } catch (err) {
-        console.error('Error updating products: ', err);
-    }
-});
+            const updatedProducts = await Promise.all(randomProducts.map(product =>
+                Product.findByIdAndUpdate(product._id, {
+                    onSaleDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+                }, { new: true })
+            ));
+
+            console.log("Updated products: ", updatedProducts);
+        } catch (err) {
+            console.error('Error updating products: ', err);
+        }
+    });
+};
