d621d598cfbe0bcf8451dc92e029b42847cca270
Justin Kao <justinkao010@gmail.com>
2025-01-13 13:14:42 -0800

    forgot to push changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import path from "node:path";
 import Stripe from "stripe";
 import cors from "cors";
 import { ChatOpenAI } from "@langchain/openai";
+import { dealsUpdate } from "./utils/dealsGenerator.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -74,6 +75,8 @@ const startApolloServer = async () => {
       res.status(500).send('Error generating AI response');
     }
   });
+
+  dealsUpdate();
 
   app.use(
     "/graphql",
--- server/src/utils/dealsGenerator.ts
+++ server/src/utils/dealsGenerator.ts
@@ -1,25 +1,24 @@
+import cron from "node-cron";
 import { Product } from "../models/index.js";
 
-const dealsUpdate = async () => {
+export const dealsUpdate = () => {
+    cron.schedule('* * * * *', async () => {
+        console.log("cron running every minute");
 
-    console.log("Starting deals update...");
+        try {
+            const expiredProducts = await Product.find({ onSaleDate: { $lt: new Date() } });
+            const randomProducts = expiredProducts.sort(() => 0.5 - Math.random()).slice(0, 9);
 
-    try {
-        const expiredProducts = await Product.find({ onSaleDate: { $lt: new Date() } });
-        const randomProducts = expiredProducts.sort(() => 0.5 - Math.random()).slice(0, 9);
+            const updatedProducts = await Promise.all(randomProducts.map(product =>
+                Product.findByIdAndUpdate(product._id, {
+                    // onSaleDate: new Date(Date.now() + 1000 * 60 * 60 * 24),
+                    onSaleDate: new Date(Date.now() + 1000 * 60),
+                }, { new: true })
+            ));
 
-        const updatedProducts = await Promise.all(randomProducts.map(product =>
-            Product.findByIdAndUpdate(product._id, {
-                onSaleDate: new Date(Date.now() + 1000 * 60 * 60 * 24),
-            }, { new: true })
-        ));
-
-        console.log("Updated products: ", updatedProducts);
-        process.exit(0);
-    } catch (err) {
-        console.error('Error updating products: ', err);
-        process.exit(1);
-    }
+            console.log("Updated products: ", updatedProducts);
+        } catch (err) {
+            console.error('Error updating products: ', err);
+        }
+    });
 };
-
-dealsUpdate();
